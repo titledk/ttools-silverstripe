0718bcf67450ca978b85b22a43253351ec1982ef
Anselm Christophersen <ac@anselm.dk>
2015-04-08 09:27:51 +0200

    Adding setup instructions to installer

--- install/install-silverstripe.sh
+++ install/install-silverstripe.sh
@@ -109,12 +109,24 @@ mkdir docs
 echo "Creating empty logs directory"
 mkdir logs
 
-echo "# Docs Readme
+echo "Creating instructions"
 
-Docs go here - you can delete this file
+echo "# Setup instructions
+
+_This project has been set up using [Terminal Tools for SilverStripe](https://github.com/titledk/ttools-silverstripe)._
+
+Follow the instructions below to get up and running with development (or readying your server for deployment):
+
+1. Clone this repo locally
+2. Set up an `_ss_environment.php` file in your root (you can copy it from `devfiles`) and edit the database settings
+3. Update your submodules: `git submodule update --init`
+4. Run `composer install` from within `public` (if you don't have composer installed, follow [these instructions](http://doc.silverstripe.org/en/getting_started/composer#installing-composer))
+5. Point a web root to `public`, and enter it
+
+You should be good to go!
 
 
-" > docs/Instructions.md;
+" > docs/setup-instructions.md;
 
 
 
