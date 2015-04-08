0050e1d09ffa3a8a0bd55d174dcfbb3c90853d38
Anselm Christophersen <ac@anselm.dk>
2015-04-08 09:51:36 +0200

    fixing some issues with the instructions creation

--- install/install-silverstripe.sh
+++ install/install-silverstripe.sh
@@ -118,10 +118,10 @@ _This project has been set up using [Terminal Tools for SilverStripe](https://gi
 Follow the instructions below to get up and running with development (or readying your server for deployment):
 
 1. Clone this repo locally
-2. Set up an `_ss_environment.php` file in your root (you can copy it from `devfiles`) and edit the database settings
-3. Update your submodules: `git submodule update --init`
-4. Run `composer install` from within `public` (if you don't have composer installed, follow [these instructions](http://doc.silverstripe.org/en/getting_started/composer#installing-composer))
-5. Point a web root to `public`, and enter it
+2. Set up an \`_ss_environment.php\` file in your root (you can copy it from \`devfiles\`) and edit the database settings
+3. Update your submodules: \`git submodule update --init\`
+4. Run \`composer install\` from within \`public\` (if you don't have composer installed, follow [these instructions](http://doc.silverstripe.org/en/getting_started/composer#installing-composer))
+5. Point a web root to \`public\`, and enter it
 
 You should be good to go!
 
