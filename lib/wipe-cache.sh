cdc86063ee963947ba5247b6a69fc5329d79f45c
Anselm Christophersen <ac@anselm.dk>
2015-07-03 07:18:20 +0200

    wipe cache script

---
+++ lib/wipe-cache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Wiping silverstripe-cache dir"
+echo "Note this only works if your user has the correct permissions"
+echo ""
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../.. && pwd )";
+MODULEDIR="$BASEDIR/ttools/silverstripe";
+SILVERSTRIPE_PATH="$BASEDIR";
+
+cd $SILVERSTRIPE_PATH;
+
+rm -rf public/silverstripe-cache/
+mkdir public/silverstripe-cache
+chmod 777 public/silverstripe-cache/
+
+echo "Done."
+
+
