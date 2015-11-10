aa1be8fed28f695a0fc4a98ebe9c2f7b62e0ddd0
Anselm Christophersen <ac@anselm.dk>
2015-11-10 20:51:37 +0100

    3.1 backward compatibility

--- install/install-silverstripe.sh
+++ install/install-silverstripe.sh
@@ -67,7 +67,9 @@ define('SS_DATABASE_SERVER', 'localhost');
 define('SS_DATABASE_USERNAME', 'root');
 define('SS_DATABASE_PASSWORD', 'root');
 define('SS_DATABASE_NAME', '$PROJECT_URLSAFE');
-define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
+
+//enable this for SilverStripe 3.2+
+//define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
 
 define('SS_ENVIRONMENT_TYPE', 'live');
 
